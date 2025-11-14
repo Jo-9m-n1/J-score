d02d367480a669d430ff9e500fecdad130e9d6af
Jongmin Lee <jongmin.lee@dawsoncollege.qc.ca>
2025-11-14 14:06:11 -0500

    Adding guestbook feature on addmission page.

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import math
 
 app = Flask(__name__)
 
+user_ans = []
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -15,7 +17,7 @@ def r_score():
 def admissions():
     return render_template("admissions.html")
 
-@app.route("/admissions/result_ad", methods=["POST"])
+@app.route("/result_ad", methods=["POST"])
 def result_ad():
     try:
         university = request.form.get("university")
@@ -43,3 +45,28 @@ def explanation():
 @app.route("/result", methods=["POST"])
 def result():
     return render_template("result.html")
+
+@app.route("/guest", methods=["POST"])
+def guest():
+    required = ["nickname", "country", "greeting"]
+    empty = []
+    for field in required:
+        if not request.form.get(field).replace(" ", ""):
+            empty.append(field)
+    if len(empty) != 0:
+        return render_template("guest_error.html", empty=empty)
+    else:
+        global nickname
+        nickname = request.form.get("nickname")
+        country = request.form.get("country")
+        greeting = request.form.get("greeting")
+        return render_template("guest.html",
+                                nickname=nickname,
+                                country=country,
+                                greeting=greeting)
+    
+@app.route("/guestbook")
+def guestbook():
+    user_ans.append(nickname)
+    print(user_ans)
+    return render_template("guestbook.html", user_ans=user_ans)
