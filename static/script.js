412a56c5847bdfab9a0c29449ebb76c3bc32aca0
Jo-9m-n1 <leejongmin1092@gmail.com>
2026-06-08 21:31:07 -0400

    feat: add dark and light mode button

--- static/script.js
+++ static/script.js
@@ -18,3 +18,19 @@ function zoomOut(e) {
   const img = e.target;
   img.classList.toggle("zoom");
 }
+
+const toggleBtn = document.getElementById('theme-toggle');
+const themeLink = document.getElementById('theme-link');
+
+const lightTheme = "https://newcss.net/theme/light.css";
+const nightTheme = "https://newcss.net/theme/night.css";
+
+toggleBtn.addEventListener('click', () => {
+  if (themeLink.getAttribute('href') === lightTheme) {
+    themeLink.setAttribute('href', nightTheme);
+    toggleBtn.textContent = "Dark Mode";
+  } else {
+    themeLink.setAttribute('href', lightTheme);
+    toggleBtn.textContent = "Light Mode";
+  }
+});
